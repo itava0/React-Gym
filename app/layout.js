973d2fb056520222087018612a1fd6cf12fdd7d6
Ivan Tavarez <ivantavarez2@gmail.com>
2023-04-21 16:19:53 -0400

    Created a footer component

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,20 @@
 import './globals.css';
-import Navbar from '../components/Nav/Navbar';
-import Hero from '../components/Hero/Hero';
-import About from '../components/About/About';
 
 export const metadata = {
   title: 'React Gym',
-  description: 'A gym is a large room, usually containing special equipment, where people go to do physical exercise and get fit. The gym has exercise bikes and running machines. ',
+  description:
+    'A gym is a large room, usually containing special equipment, where people go to do physical exercise and get fit. The gym has exercise bikes and running machines. ',
+  icons: {
+    icon: '/favicon.png',
+    sizes: "180x180",
+    type: "image/png"
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-       <title>React Gym</title>
-    </head>
-      <body>
-      <Navbar />
-      <Hero />
-      <About />
-         {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
