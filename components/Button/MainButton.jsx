68acb34341fa1dfe1f34d7d2f4be49a253aac3f9
Ivan Tavarez <ivantavarez2@gmail.com>
2023-04-27 11:11:07 -0400

    Updated the mainbutton

--- components/Button/MainButton.jsx
+++ components/Button/MainButton.jsx
@@ -4,7 +4,7 @@ const MainButton = ({ color, bg, cN, arrowColor, hover, text, goTo }) => {
   return (
     <Link
       href={goTo}
-      className={`text-[15px]  ${color} ${bg} ${cN} text-center pt-[18px] font-[600] w-[20rem] h-[5.5rem] uppercase hero-cta relative ml-2 ${hover}`}
+      className={`text-[15px]  ${color} ${bg} ${cN} text-center pt-[18px] font-[600] w-[20rem] h-[5.5rem] uppercase relative ml-2 ${hover}`}
     >
       {text} &nbsp;
       <i className={`fa-solid fa-arrow-right text-[#FF0336] ${arrowColor}`}></i>
