11c78ed0f1a8ab87a887f5c0f8466324a333a7bc
Ivan Tavarez <ivantavarez2@gmail.com>
2023-04-27 11:10:36 -0400

    Created a membership cards for the plan

---
+++ components/Membership/MembershipCardList.js
@@ -0,0 +1,35 @@
+export const MembershipCardList = [
+    {
+        plan:"Starter",
+        price:"10",
+        amenities:
+            {
+                0: "Unlimited Home Club Access",
+                1: "Free WiFi",
+                2: "Access to online classes",
+                3: "Sauna"
+            }
+    },
+    {
+        plan:"Pro",
+        price:"15",
+        amenities: 
+            {
+                0: "Access to all Starter amenities",
+                1: "Health & fitness guides",
+                2: "Kids Club",
+                3: "Use Of Basketball Courts"
+            }
+    },
+    {
+        plan:"Premium",
+        price:"25",
+        amenities:
+            {
+                0: "Access to all Pro amenities",
+                1: "Full diet plan",
+                2: "Full customize plan",
+                3: "Daily progress check up"
+            }
+    }
+]
