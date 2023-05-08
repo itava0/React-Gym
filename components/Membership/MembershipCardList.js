6d1cdd1bd95d52c3b223bfc63c082015c4b604c7
Ivan Tavarez <ivantavarez2@gmail.com>
2023-05-08 14:21:55 -0400

    Add priceId from Stripe

--- components/Membership/MembershipCardList.js
+++ components/Membership/MembershipCardList.js
@@ -1,35 +1,35 @@
 export const MembershipCardList = [
-    {
-        plan:"Starter",
-        price:"10",
-        amenities:
-            {
-                0: "Unlimited Home Club Access",
-                1: "Free WiFi",
-                2: "Access to online classes",
-                3: "Sauna"
-            }
+  {
+    plan: 'Basic',
+    price: '10',
+    Price_ID: 'price_1N5UxJGuPVBl9todtq5XSvVP',
+    amenities: {
+      0: 'Unlimited Home Club Access',
+      1: 'Free WiFi',
+      2: 'Access to online classes',
+      3: 'Sauna',
     },
-    {
-        plan:"Pro",
-        price:"15",
-        amenities: 
-            {
-                0: "Access to all Starter amenities",
-                1: "Health & fitness guides",
-                2: "Kids Club",
-                3: "Use Of Basketball Courts"
-            }
+  },
+  {
+    plan: 'Pro',
+    price: '15',
+    Price_ID: 'price_1N5UymGuPVBl9todQYbnyVhy',
+    amenities: {
+      0: 'Access to all Starter amenities',
+      1: 'Health & fitness guides',
+      2: 'Kids Club',
+      3: 'Use Of Basketball Courts',
     },
-    {
-        plan:"Premium",
-        price:"25",
-        amenities:
-            {
-                0: "Access to all Pro amenities",
-                1: "Full diet plan",
-                2: "Full customize plan",
-                3: "Daily progress check up"
-            }
-    }
-]
+  },
+  {
+    plan: 'Premium',
+    price: '25',
+    Price_ID: 'price_1N5UzbGuPVBl9tod8IHo6Ikd',
+    amenities: {
+      0: 'Access to all Pro amenities',
+      1: 'Full diet plan',
+      2: 'Full customize plan',
+      3: 'Daily progress check up',
+    },
+  },
+];
