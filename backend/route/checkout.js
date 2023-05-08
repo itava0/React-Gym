030042d19311cc70e752a9a8fe3466ff6c848bc8
Ivan Tavarez <ivantavarez2@gmail.com>
2023-05-08 17:30:48 -0400

    Refactor URL

--- backend/route/checkout.js
+++ backend/route/checkout.js
@@ -1,6 +1,6 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); 
 
-const YOUR_DOMAIN = 'http://localhost:3000';
+const YOUR_DOMAIN = 'https://react-gym-bice.vercel.app';
 
 const submitPayment = async (req, res) => {
   const {priceId} = req.body;
