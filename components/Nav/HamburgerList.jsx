256c912fa5197d508c529eff7efb23609528af4f
Ivan Tavarez <ivantavarez2@gmail.com>
2023-05-09 18:26:19 -0400

    Fixed the nav route

--- components/Nav/HamburgerList.jsx
+++ components/Nav/HamburgerList.jsx
@@ -7,31 +7,31 @@ const HamburgerList = () => {
         className="border-b border-gray-400 my-8 uppercase cursor-pointer hover:text-[#ff0336]"
         style={{ transition: 'all 0.3s' }}
       >
-        <Link href="/">Home</Link>
+        <Link href="/" className='text-lg'>Home</Link>
       </li>
       <li
         className="border-b border-gray-400 my-8 uppercase cursor-pointer hover:text-[#ff0336]"
         style={{ transition: 'all 0.3s' }}
       >
-        <Link href="/classes">Classes</Link>
+        <Link href="/classes" className='text-lg'>Classes</Link>
       </li>
       <li
         className="border-b border-gray-400 my-8 uppercase cursor-pointer hover:text-[#ff0336]"
         style={{ transition: 'all 0.3s' }}
       >
-        <Link href="/memberships">Membership</Link>
+        <Link href="/memberships" className='text-lg'>Membership</Link>
       </li>
       <li
         className="border-b border-gray-400 my-8 uppercase cursor-pointer hover:text-[#ff0336]"
         style={{ transition: 'all 0.3s' }}
       >
-        <Link href="/stories">Stories</Link>
+        <Link href="/blog" className='text-lg'>Our Blog</Link>
       </li>
       <li
         className="border-b border-gray-400 my-8 uppercase cursor-pointer hover:text-[#ff0336]"
         style={{ transition: 'all 0.3s' }}
       >
-        <Link href="/myaccount">My Account</Link>
+        <Link href="/myaccount" className='text-lg'>My Account</Link>
       </li>
     </ul>
   );
