c3287cf9ad7e62c589301270af76e090840aa90b
Ivan Tavarez <ivantavarez2@gmail.com>
2023-05-15 18:05:20 -0400

    Created a new endpoint call auth/register

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
-const cors = require("cors"); 
-const express = require("express"); 
-require("dotenv").config(); 
+const cors = require('cors');
+const express = require('express');
+require('dotenv').config();
 const app = express();
 const db = require('./queries');
 const payment = require('./route/checkout');
 const subscription = require('./route/webhook');
+const register = require('./route/auth');
 const port = process.env.PORT || 8080;
 
-// Middlewares here 
-app.use(express.static("public"));
-app.use(express.json()); 
-app.use(cors()); 
+// Middlewares here
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 
-
-// Routes here 
+// Routes here
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' });
 });
@@ -32,6 +32,7 @@ app.delete('/users/:id', db.deleteUser);
 app.post('/create-checkout-session', payment.submitPayment);
 app.post('/webhook', subscription.manageSubcription);
 
+app.post('/auth/register', register.registeredUser);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
