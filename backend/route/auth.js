9c2cdf998f10c4d6000e5f55a6dab519dc65b461
Ivan Tavarez <ivantavarez2@gmail.com>
2023-05-15 18:22:45 -0400

    Created loginUser func to auth user

--- backend/route/auth.js
+++ backend/route/auth.js
@@ -1,5 +1,6 @@
 const pool = require('./config');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Registration endpoint
 const registeredUser = async (req, res) => {
@@ -33,6 +34,48 @@ const registeredUser = async (req, res) => {
   }
 };
 
+// JWT secret key
+const jwtSecret = 'your_jwt_secret';
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user with matching email
+    const query = 'SELECT * FROM users WHERE email = $1';
+    const values = [email];
+    const result = await pool.query(query, values);
+
+    if (result.rows.length === 0) {
+      // User not found
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+
+    const user = result.rows[0];
+
+    // Compare the provided password with the hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      // Create JWT token
+      const token = jwt.sign({ userId: user.id }, jwtSecret, {
+        expiresIn: '1h',
+      });
+
+      // Authentication successful
+      return res.json({ token });
+    } else {
+      // Invalid password
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+  } catch (error) {
+    // Handle any error that occurred during the authentication process
+    console.error('Authentication error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 module.exports = {
   registeredUser,
+  loginUser,
 };
