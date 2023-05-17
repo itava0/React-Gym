bfae2334a96a5726695e315d10c5668d620e38c6
Ivan Tavarez <ivantavarez2@gmail.com>
2023-05-17 14:16:35 -0400

    Add the multiple set fucn to update the state.

--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -1,7 +1,8 @@
 "use client";
 import { useState } from 'react';
 
-function Profile({setFirstName, setLastName, setBirthdate, setGender, setAddress, setPhoneNumber, setEmail}) {
+function Profile({setFirstName, setLastName, setBirthdate, setGender, setHomeAddress, setCity, setState, setZipCode, setPhoneNumber, setEmail}) {
+
   return (
     <div>
       <div className="w-full bg-white px-10">
@@ -81,7 +82,7 @@ function Profile({setFirstName, setLastName, setBirthdate, setGender, setAddress
               tabIndex={0}
               aria-label="Enter Home Address"
               className="w-64 mb-3 bg-gray-100 text-md font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
-              onChange={(e) => setAddress(e.target.value)}
+              onChange={(e) => setHomeAddress(e.target.value)}
             />
           </div>
           <div className="flex flex-col md:ml-12 md:mt-0 mt-8">
@@ -93,7 +94,7 @@ function Profile({setFirstName, setLastName, setBirthdate, setGender, setAddress
               tabIndex={0}
               aria-label="Enter City Address"
               className="w-64 mb-3 bg-gray-100 text-md font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
-              onChange={(e) => setAddress(e.target.value)}
+              onChange={(e) => setCity(e.target.value)}
             />
           </div>
         </div>
@@ -107,7 +108,7 @@ function Profile({setFirstName, setLastName, setBirthdate, setGender, setAddress
               tabIndex={0}
               aria-label="Enter State Address"
               className="w-64 mb-3 bg-gray-100 text-md font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
-              onChange={(e) => setAddress(e.target.value)}
+              onChange={(e) => setState(e.target.value)}
             />
           </div>
           <div className="flex flex-col md:ml-12 md:mt-0 mt-8">
@@ -119,7 +120,7 @@ function Profile({setFirstName, setLastName, setBirthdate, setGender, setAddress
               tabIndex={0}
               aria-label="Enter Zip Code Address"
               className="w-64 mb-3 bg-gray-100 text-md font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
-              onChange={(e) => setAddress(e.target.value)}
+              onChange={(e) => setZipCode(e.target.value)}
             />
           </div>
         </div>
@@ -129,14 +130,14 @@ function Profile({setFirstName, setLastName, setBirthdate, setGender, setAddress
               Date of birth
             </label>
             <input
-              type
+              type="date"
               tabIndex={0}
               aria-label="Enter date of birth"
               className="w-64 mb-3 bg-gray-100 text-md font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
               onChange={(e) => setBirthdate(e.target.value)}
             />
           </div>
-          <div className="flex flex-col md:ml-12 md:mt-0 mt-8" onChange={(e) => setGender(e.target.value)}>
+          <div className="flex flex-col md:ml-12 md:mt-0 mt-8">
             <label className="mb-3 text-lg leading-none text-gray-800">
               Gender
             </label>
@@ -147,6 +148,7 @@ function Profile({setFirstName, setLastName, setBirthdate, setGender, setAddress
                   type="radio"
                   value="Male"
                   name="gender"
+                  onChange={(e) => setGender(e.target.value)}
                   className="checkbox mb-3 appearance-none focus:outline-none border rounded-full border-gray-400 absolute cursor-pointer w-full h-full checked:border-none"
                 />
                 <div className="check-icon hidden border-4 border-indigo-700 rounded-full w-full h-full z-1" />
@@ -163,6 +165,7 @@ function Profile({setFirstName, setLastName, setBirthdate, setGender, setAddress
                   type="radio"
                   value="Female"
                   name="gender"
+                  onChange={(e) => setGender(e.target.value)}
                   className="checkbox  mb-3 appearance-none focus:outline-none border rounded-full border-gray-400 absolute cursor-pointer w-full h-full checked:border-none"
                 />
                 <div className="check-icon hidden border-4 border-indigo-700 rounded-full w-full h-full z-1" />
@@ -179,6 +182,7 @@ function Profile({setFirstName, setLastName, setBirthdate, setGender, setAddress
                   type="radio"
                   value="Other"
                   name="gender"
+                  onChange={(e) => setGender(e.target.value)}
                   className="checkbox  mb-3 appearance-none focus:outline-none border rounded-full border-gray-400 absolute cursor-pointer w-full h-full checked:border-none"
                 />
                 <div className="check-icon hidden border-4 border-indigo-700 rounded-full w-full h-full z-1" />
