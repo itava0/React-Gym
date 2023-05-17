8ff76d1b29387c906e16adbc53efe2fa1fb77582
Ivan Tavarez <ivantavarez2@gmail.com>
2023-05-17 15:27:19 -0400

    Add the connection to Postgres on AWS

--- backend/config.js
+++ backend/config.js
@@ -1,31 +1,31 @@
-// const pg = require('pg');
-// const conString = "postgres://nbwrnbuh:XmOpHFBnXD9NDXMEThGrNE6tzHbG8oEf@drona.db.elephantsql.com/nbwrnbuh"
-// const client = new pg.Client(conString);
+const pg = require('pg');
+const conString = "postgres://nbwrnbuh:XmOpHFBnXD9NDXMEThGrNE6tzHbG8oEf@drona.db.elephantsql.com/nbwrnbuh"
+const client = new pg.Client(conString);
 
 
-// client.connect(function(err) {
-//   if(err) {
-//     return console.error('could not connect to postgres', err);
-//   }
-//   client.query('SELECT NOW() AS "theTime"', function(err, result) {
-//     if(err) {
-//       return console.error('error running query', err);
-//     }
-//     console.log(result.rows[0].theTime);
-//     // >> output: 2018-08-23T14:02:57.117Z
-//   });
-// });
-
-// module.exports = client;
-
-//Local enviroment
-const Pool = require('pg').Pool;
-const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DB,
-  password: process.env.PASSWORD,
-  port: 5432
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+  });
 });
 
-module.exports = pool;
+module.exports = client;
+
+//Local enviroment
+// const Pool = require('pg').Pool;
+// const pool = new Pool({
+//   user: process.env.USER,
+//   host: process.env.HOST,
+//   database: process.env.DB,
+//   password: process.env.PASSWORD,
+//   port: 5432
+// });
+
+// module.exports = pool;
