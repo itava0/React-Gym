f368c1333de870b48b23200d94687da7f024c7cb
Ivan Tavarez <ivantavarez2@gmail.com>
2023-05-17 18:24:41 -0400

    Remove sensite key

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental:{appDir: true},
-  env: {
-    STRIPE_PUBLISHABLE_KEY:'pk_test_51N3jdYGuPVBl9todAf2jn3AxE54HzD8AijUaWx3kzAkj9r3cUg6tl0BVwiUWlAOfLRwuno9YckMAjYOL3biUTyBu00SyISOg1h',
-  },
 }
 
 module.exports = nextConfig
